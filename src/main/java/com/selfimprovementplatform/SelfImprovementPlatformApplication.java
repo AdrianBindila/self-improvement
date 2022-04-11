663c9944139697c8c0b61549b66d2960362f5ba2
Adrian <bindila.ioan@gmail.com>
2022-04-11 20:06:54 +0300

    Initial commit

---
+++ src/main/java/com/selfimprovementplatform/SelfImprovementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.selfimprovementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfImprovementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfImprovementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/selfimprovementplatform/SelfImprovementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfimprovementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfImprovementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
